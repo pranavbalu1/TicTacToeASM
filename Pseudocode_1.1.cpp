0658b0300f002169331a6abf500c899c5a977648
KevDev7 <96637803+KevDev7@users.noreply.github.com>
2023-03-28 17:50:35 -0500

    Update Pseudocode_1.1.cpp

--- Pseudocode_1.1.cpp
+++ Pseudocode_1.1.cpp
@@ -5,37 +5,35 @@
 
 main:
    askUserXorO();              // Ask the user whether they rather play as 'X' or 'O'
-                ;              // $s0 = userChar & $s1 = compChar
+                               // $s0 = userChar & $s1 = compChar
    int boardMoves = 0;         // The number of moves done by user and computer
    bool didTieHappen = false;  // Whether a tie existing is true or false.
    
-   if(user == 'X')
-   {
+   if(user == 'X') {
       branch to xWhileLoop
    }
-   if(user == 'O')
-   {
+   if(user == 'O') {
       branch to oWhileLoop
    }
    
-   
-   while()   // x while loop
-   {
+   while() {  // x while loop
       displayBoardMenu();  // Displays the board with numbers (1-9) on board placements
       getUserInput();      // Ask user to input their move on board (1-9) and save into board
       userMoves++;         // Increment number of board moves
       displayFullBoard();  // Displays the board with 'X', 'O', and ' ' on the board pieces
       isWinYet();          // Check if a win exist diagonally, vertically, or horizontally
       
-      if(userMoves == 9) { break;}
+      if(userMoves == 9) {
+         branch to exitWhileLoop;
+      }
       
       getComputerMove();   // Get the computer to place their move on empty placements
       userMoves++;        // Increment number of board moves
       displayFullBoard();  // Displays the board with 'X', 'O', and ' ' on the board pieces
       isWinYet();          // Check if a win exist diagonally, vertically, or horizontally
    }
-   while()   // o while loop
-   {
+
+   while() {  // o while loop
       getComputerMove();   // Get the computer to place their move on empty placements
       boardMoves++;        // Increment number of board moves
       displayFullBoard();  // Displays the board with 'X', 'O', and ' ' on the board pieces
@@ -47,7 +45,9 @@ main:
       displayFullBoard();  // Displays the board with 'X', 'O', and ' ' on the board pieces
       isWinYet();          // Check if a win exist diagonally, vertically, or horizontally
       
-      if(userMoves == 9) { break;}
+      if(userMoves == 9){
+         branch to exitWhileLoop;
+      }
       
       displayBoardMenu();  // Displays the board with numbers (1-9) on board placements
       getUserInput();      // Ask user to input their move on board (1-9) and save into board
@@ -56,22 +56,20 @@ main:
       isWinYet();          // Check if a win exist diagonally, vertically, or horizontally
    }
    
-   branchfrombreak
+   exitWhileLoop:
    
    didTieHappen = true;    // Getting out while-loop indicates that tie did occur.
    
-   if(didTieHappen == true)
-   {
+   if(didTieHappen == true) {
       displayTieMessage(); // Display that there has been a tie.
    }
-   else
-   {
+   else {
       displayWinMessage(); // Display a message saying who won the game.
    }
    
    // Exit the program
 
-;/////////////////// END OF MAIN PROGRAM ///////////////////////////////////////
+/////////////////// END OF MAIN PROGRAM ///////////////////////////////////////
 
 isWinYet()
 {
@@ -80,14 +78,12 @@ isWinYet()
    isWinHorizontally(); // IF win found, branch to displayWin();
 }
 
-getComputerMove()
-{
-   ;// Somewhere in this code you will have to check whether the board placement is free to have the computer move on.
-   ;// So, please make a seperate function like isBoardSpotFree() so it can be used for other parts of the code.
-   ;// Have it return a bool (true or false) on whether the board placement is free or not.
+getComputerMove() {
+   // Somewhere in this code you will have to check whether the board placement is free to have the computer move on.
+   // So, please make a seperate function like isBoardSpotFree() so it can be used for other parts of the code.
+   // Have it return a bool (true or false) on whether the board placement is free or not.
 }
 
-isBoardSpotFree()
-{
+isBoardSpotFree() {
    
 }
